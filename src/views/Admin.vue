6911de26c6f2e8d36f165e41100fe1a80a233c0e
weihung-1010 <eagle20150813@gmail.com>
2022-10-06 23:09:13 +0800

    edit-login-page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -8,7 +8,7 @@
         alt="LogoImage"
       />
     </div>
-    <h3 class="text-center font-weight-bold ">後台登入</h3>
+    <h3 class="text-center font-weight-bold mt-4">後台登入</h3>
 
     <form @submit.prevent.stop="handleSubmit">
       <div class="form-wrapper mt-5">
