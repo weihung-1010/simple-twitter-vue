e28833c70a6048c4a1260c1b8c4bafa18a4c4be5
Yuzu <yuzuprocoding@gmail.com>
2022-10-07 23:17:16 +0800

    add mixin filters

---
+++ src/components/AdminTweetsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tweets-list">
+    <div class="list-title">
+      <h4>推文清單</h4>
+    </div>
+    //測試濾鏡功能
+    <img class="avatar" :src="currentUser.avatar | emptyImage" alt="avatar" />
+    <div>{{ createdAt | fromNow }}</div>
+  </div>
+</template>
+
+
+<script>
+import { fromNowFilter, emptyImageFilter } from "./../utils/mixins";
+
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: "user1",
+    email: "user1@example.com",
+    avatar: "",
+    isAdmin: false,
+  },
+  isAuthenticated: true,
+};
+export default {
+  mixins: [fromNowFilter, emptyImageFilter],
+  data() {
+    return {
+      currentUser: dummyUser.currentUser,
+      createdAt: "2019-06-07T13:28:51.000Z",
+    };
+  },
+};
+</script>
